45aa141d41d6c2605dccc9f1dd7ebc9827104257
Richard <you@example.com>
2024-03-12 01:59:49 +0100

    [Add] added order controller

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, ParseIntPipe, NotFoundException } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
@@ -17,44 +17,48 @@ export class OrderController {
     @Query('page', ParseIntPipe) page: number,
     @Query('limit', ParseIntPipe) limit: number,
   ) {
+    // return "richard";
     return this.orderService.findAll(page, limit);
   }
 
   @Get('/seller_orders')
-  async findBySellerId(
-    @Query('seller_id',) seller_id: string,
+  findBySellerId(
     @Query('page', ParseIntPipe) page: number,
     @Query('limit', ParseIntPipe) limit: number,
+    @Query('seller_id',) seller_id: string,
   ) {
-    return this.orderService.findBySellerId(seller_id, page, limit);
+    return this.orderService.findBySellerId(page, limit, seller_id);
   }
 
   @Get('/buyer_orders')
-  async findByBuyerId(
-    @Query('buyer_id',) buyer_id: string,
+  findByBuyerId(
     @Query('page', ParseIntPipe) page: number,
     @Query('limit', ParseIntPipe) limit: number,
+    @Query('buyer_id',) buyer_id: string,
   ) {
-    return this.orderService.findByBuyerId(buyer_id, page, limit);
+    if (!buyer_id) {
+      throw new NotFoundException('No record found');
+    }
+    return this.orderService.findByBuyerId(page, limit, buyer_id);
   }
 
   @Get('/buyer_orders_by_email')
-  async findByBuyerEmail(
-    @Query('buyer_email',) buyer_email: string,
+  findByBuyerEmail(
     @Query('page', ParseIntPipe) page: number,
     @Query('limit', ParseIntPipe) limit: number,
+    @Query('buyer_email',) buyer_email: string,
   ) {
-    return this.findByBuyerEmail(buyer_email, page, limit);
+    return this.orderService.findByBuyerEmail(page, limit, buyer_email);
   }
 
 
   @Get('/location')
-  async findByLocation(
-    @Query('location',) location: string,
+  findByLocation(
     @Query('page', ParseIntPipe) page: number,
     @Query('limit', ParseIntPipe) limit: number,
+    @Query('location',) location: string,
   ) {
-    return this.findByBuyerEmail(location, page, limit)
+    return this.orderService.findByLocation(page, limit, location)
   }
 
   @Get(':id')
